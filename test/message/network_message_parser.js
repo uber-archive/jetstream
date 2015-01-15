ad55f17dfa88a07228a749322f479e55827a171a
Rob Skillington <r@uber.com>
2015-01-15 13:50:40 -0800

    Adding strict for all source files

--- test/message/network_message_parser.js
+++ test/message/network_message_parser.js
@@ -21,6 +21,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+'use strict';
 
 var createTestContext = require('../test/test_context');
 var NetworkMessageParser = require('../../lib/message/network_message_parser');
